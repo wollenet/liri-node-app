ff3e3d2407c401831caec7cc711c62892796056c
wollenet <30678260+wollenet@users.noreply.github.com>
2018-01-08 16:17:24 -0500

    Add files via upload

---
+++ liri_bot_app/Keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+exports.twitter = {
+  consumer_key: eZXqSL2RaL1eHYDxDZCnzOnhE,
+  consumer_secret: OQPmb4C0ruVy7YaJD2q3TUVkvWfGl1q2XRBYgfe05tOhIsuHxM,
+  access_token_key: 	950401932165812225-TSTdA7SIujFpBrHohQ1npzNST1Lvf6s,
+  access_token_secret:	950401932165812225-TSTdA7SIujFpBrHohQ1npzNST1Lvf6s
+};
+
+exports.tmdbKey = '4973d83a';
